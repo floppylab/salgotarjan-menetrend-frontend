f89d552dfe0beccd1662c14fdf7bf00598e72d0e
leonorader <dernori@gmail.com>
2020-08-12 22:51:45 +0200

    mailto link to bug report

--- pages/informacio.vue
+++ pages/informacio.vue
@@ -9,7 +9,7 @@
         Menetrend érvényes 2019. 04. 15-től
       </p>
       <small v-if="info">v{{ info.version }} - {{ info.buildTime }}</small>
-      <p>hibabejelentés: <a href="mailto:info@floppylab.com?subject=hibajelent%C3%A9s%20-%20Salg%C3%B3tarj%C3%A1n%20menetrend%20alkalmaz%C3%A1s&body=Kedves%20Fejleszt%C5%91k!%0AAz%20al%C3%A1bbi%20hib%C3%A1t%20tal%C3%A1ltam%2C%20k%C3%A9rn%C3%A9m%20kijav%C3%ADt%C3%A1s%C3%A1t%3A%0A%0AK%C3%B6sz%C3%B6nettel%2C">info@floppylab.com</a></p>
+      <p>hibabejelentés: <a href="mailto:info@floppylab.com?subject=hibajelent%C3%A9s%20-%20Salg%C3%B3tarj%C3%A1n%20menetrend%20alkalmaz%C3%A1s&body=Kedves%20Fejleszt%C5%91k!%0AAz%20al%C3%A1bbi%20hib%C3%A1t%20tal%C3%A1ltam%2C%20k%C3%A9rem%20jav%C3%ADt%C3%A1s%C3%A1t%3A%0A%0AK%C3%B6sz%C3%B6nettel%2C">info@floppylab.com</a></p>
       <p>2020 <a href="https://floppylab.com">floppylab</a></p>
     </div>
   </main>
